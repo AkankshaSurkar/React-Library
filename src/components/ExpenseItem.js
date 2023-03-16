547048405489f3b8098d255c4d1c1044be35bb0c
AkankshaSurkar <akankshasfdc96@gmail.com>
2023-03-16 16:02:58 +0530

    commit msg

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (
+       <div>
+        <div>Expense Item</div>
+        <div>
+            <h3>Food Rs10</h3>
+             <h3>Movie Rs100</h3>
+            <h3>Petrol Rs200</h3>
+        </div>
+       </div>
+    );
+}
+export default ExpenseItem;
