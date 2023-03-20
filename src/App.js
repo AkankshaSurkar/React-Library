dc3441635aea057b4ff4bede1bf6f8f09a6bfce9
AkankshaSurkar <akankshasfdc96@gmail.com>
2023-03-20 14:56:42 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expenses';
 import ExpenseItem from './components/ExpenseItem';
 
-function App() {
+const App=()=> {
   
       // <div className="App">
       //   <header className="App-header">
@@ -10,66 +12,47 @@ function App() {
         {
           id:'e1',
           title: 'Toilet Paper',
-          amount: '$94.12',
+          amount: '94.12',
           date: new Date(2020,7,14),
           location: 'Maxico',
         },
                 {
            id: 'e2',       
           title: 'New TV',
-          amount: '$79992',
+          amount: '79992',
           date: new Date(2020,8,9),
           location: 'flipcart',
         },
                 {
           id: 'e3',
           title: 'Car Insurance',
-          amount: '$1000',
+          amount: '1000',
           date: new Date(2020,9,20),
           location:'Hondo Showroom',
         },
             {
           id:'e4',
           title: 'New Desk (wooden)',
-          amount: '$790',
+          amount: '790',
           date: new Date(2020,8,5),
           location:'Deradun',
         },
       ];
-        return (
-          <div>
-            <ExpenseItem 
-            title ={expenses[0].title}
-            amount = {expenses[0].amount}
-            date = {expenses[0].date}
-            location = {expenses[0].location}
-            ></ExpenseItem>
-
-             <ExpenseItem 
-            title ={expenses[1].title}
-            amount = {expenses[1].amount}
-            date = {expenses[1].date}
-            location = {expenses[1].location}
-
-            ></ExpenseItem>
-
-             <ExpenseItem 
-            title ={expenses[2].title}
-            amount = {expenses[2].amount}
-            date = {expenses[2].date}
-            location = {expenses[2].location}
-
-            ></ExpenseItem>
-
-        <ExpenseItem 
-            title ={expenses[3].title}
-            amount = {expenses[3].amount}
-            date = {expenses[3].date}
-            location = {expenses[3].location}
-
-            ></ExpenseItem>
-          </div>
+        return React.createElement('div', {},
+        React.createElement('h2',{}, "Lets get started!"),
+        React.createElement(Expenses,{items:expenses})
         );
+
+        
+          
+          
+//          <div>
+//           <h2>Let get strted</h2>
+//           <Expenses items={expenses}
+//            />
+// </div>
+
+        // );
 
       }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,55 +1,17 @@
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
+import Card from './Card';
 import './ExpenseItem.css';
-function ExpenseItem(props){
-    const expenseDate = new Date(2021,2,28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-    const expenseLocation ='Honda Showroom Nagpur';
-    
-        return (
-            
-           <div className='expense-item'>
-            <ExpenseDate date={props.date} />
-           <div className='expense-item_description'>
-           <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-            {/* <h2>{props.title}</h2>
-            <div className='expense-item_price'>${props.amount}</div>
-            <div className='expense-item_place'>
-           <div>{props.LcommitocationOfExpenditure}</div>
-            </div> */}
 
-    
-           </div>
-           </div>
-        );
-    }
-    export default ExpenseItem;
-
-
-
-
-
-
-
-// function ExpenseItem(){
-// const expenseDate = new Date(2021,2,28);
-// const expenseTitle = 'Car Insurance';
-// const expenseAmount = 294.67;
-// const LocationOfExpenditure ='Honda Showroom Nagpur';
-
-//     return (
-//        <div className='expense-item'>
-//        <div>{expenseDate.toISOString()}</div>
-//        <div className='expense-item_description'>
-//         <h2>{expenseTitle}</h2>
-//         <div className='expense-item_price'>${expenseAmount}</div>
-//         <div className='expense-item_place'>
-//         <div>{LocationOfExpenditure}</div>
-//             </div>
-
-//        </div>
-//        </div>
-//     );
-// }
-// export default ExpenseItem;
+const ExpenseItem =(props)=>{
+    return(
+        <Card className='expense-item'>
+            <ExpenseDate date = {props.date}/>
+            <div className = 'expense-item_description'>
+                <h2>{props.title}</h2>
+                <div className ='expense-item_price'>${props.amount}</div>
+            </div>
+        </Card>
+    );
+}
+export default ExpenseItem;
