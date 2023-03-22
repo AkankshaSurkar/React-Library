5c813f9b1f79eddfaff669436a23766bfc42e134
AkankshaSurkar <akankshasfdc96@gmail.com>
2023-03-22 08:05:43 +0530

    commited

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import  {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 
-const ExpenseItem =(props)=>{
+ const ExpenseItem =(props)=>{
+
+const [title, setTitle]=useState(props.title);
+const [visible,setVisible]=useState(true);
+
+const removeElement=()=>{
+    setVisible((expense)=>!expense);
+    console.log(visible);
+};
+     const clickHandler =() =>{
+        setTitle('Updated');
+        console.log(title);
+    };
+    
     return(
         <Card className='expense-item'>
             <ExpenseDate date = {props.date}/>
             <div className = 'expense-item_description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className ='expense-item_price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
+            <button onClick={removeElement}>DeleteExpense</button>
+            
+
+
         </Card>
+
     );
 }
 export default ExpenseItem;
